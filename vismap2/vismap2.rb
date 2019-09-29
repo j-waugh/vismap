8ef4f1a5919282b1643ca2d46415cb896ab10d29
Jeremy Waugh <jwaugh@gmail.com>
2019-09-29 18:38:23 -0400

    add plugin reload function

--- vismap2/vismap2.rb
+++ vismap2/vismap2.rb
@@ -11,6 +11,20 @@ If there are 5 scenes, the first five letters in the string are the "V/I" settin
 
 module Vismap
 
+# Reload extension by running this method from the Ruby Console:
+#   Example::HelloWorld.reload
+#   def self.reload
+#     original_verbose = $VERBOSE
+#     $VERBOSE = nil
+#     pattern = File.join(__dir__, '**/*.rb')
+#     Dir.glob(pattern).each { |file|
+#       # Cannot use `Sketchup.load` because its an alias for `Sketchup.require`.
+#       load file
+# 	}.size
+#   ensure
+#     $VERBOSE = original_verbose
+#   end
+
 class Vismap_Model
 
 	def initialize()
